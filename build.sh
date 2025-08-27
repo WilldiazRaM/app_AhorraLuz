4e4fa003d88613c3b6310906ef9b3e3a174d36dd
WilldiazRaM <willdiazsantander@gmail.com>
2025-08-27 08:41:17 -0400

    Proyecto Django listo para Render

---
+++ build.sh
@@ -0,0 +1,11 @@
+cat > build.sh << 'EOF'
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
+EOF
+
+chmod a+x build.sh
+git update-index --chmod=+x build.sh
